be4c64bed280c68e223d5f6b38ad00a4e3bfbd41
qwasfl <worivok@gmail.com>
2024-10-12 11:15:42 +0300

    1

---
+++ config.py
@@ -0,0 +1,4 @@
+API_TOKEN = '7354057643:AAFAm7VztBUfgZ1mcHTpgRrTfhzwYJbBsq4' #TOKEN бота
+BOT_USERNAME = 'https://t.me/HiddenSMSru_bot' # https://t.me/BOTUSERNAME Нужна для генерации реферальной ссылки
+ADMINS_ID = [1956728833] # можно добавить много через запятую. Пример  [798661023, 798661023, 798661023]
+#DATABASE
